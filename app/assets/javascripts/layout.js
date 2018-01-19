99bf697ab4ebf85df3290da7e3ffafec1abccef2
AdminAcens <projetos.acens@gmail.com>
2018-01-19 12:43:19 +0500

    finish MVP

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,6 +1,6 @@
 var Layout = function () {
     'use strict';
-    
+
     // handle on page scroll
     var handleHeaderOnScroll = function() {
         if ($(window).scrollTop() > 60) {
@@ -8,14 +8,14 @@ var Layout = function () {
         } else {
             $('body').removeClass('page-on-scroll');
         }
-    }   
+    }
 
     // handle carousel
     var handleCarousel = function() {
-        var $item = $('.carousel .item'); 
+        var $item = $('.carousel .item');
         var $wHeight = $(window).height();
         $item.eq(0).addClass('active');
-        $item.height($wHeight); 
+        $item.height($wHeight);
         $item.addClass('full-screen');
 
         $('.carousel img').each(function() {
@@ -88,4 +88,9 @@ var Layout = function () {
 
 $(document).ready(function() {
     Layout.init();
+});
+
+$(window).load(function() {
+    // Animate loader off screen
+    $(".page-loader").fadeOut("slow");
 });
